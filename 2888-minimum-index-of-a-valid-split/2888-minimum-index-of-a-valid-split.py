0801fdf7f009b24f207cce7dc3f2694009c9b888
pkritika <129911259+pkritika@users.noreply.github.com>
2025-03-27 02:10:07 -0400

    Time: 72 ms (50.99%), Space: 34.4 MB (47.53%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.py
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.py
@@ -2,20 +2,21 @@ class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
         freq = {}
         for num in nums:
-            freq[num] = freq.get(num, 0) + 1
+            freq[num] = freq.get(num, 0) + 1   #freq = {1: 1, 2: 3}
         dominant = None
         max_count = 0
 
         for key, value in freq.items():
             if value > max_count:
                 max_count = value
-                dominant = key
+                dominant = key #since the value of 2 is more than other value we take 2 as our dominant number and the total count is the 3
 
-        total_count = max_count 
+        total_count = max_count   #Dominant element = 2
+                                 #Total occurrences = 3
 
         prefix_count = 0
         n = len(nums)
-        for i in range(n - 1):  
+        for i in range(n - 1):    
             if nums[i] == dominant:
                 prefix_count += 1
                 total_count -= 1  
@@ -27,6 +28,22 @@ class Solution:
                 return i  
         return -1 
         
+
+# iteration 1 (i = 0, nums[i] = 1)
+        #nums[0] = 1, which is not the dominant element.So, prefix_count and total_count remain unchanged.
+        # Left subarray: [1] → 2 is not dominant
+        # Right subarray: [2, 2, 2] → 2 is dominant, but left is invalid.
+        # No valid split at i = 0
+#iteration 2 (i =1 , nums [i] = 2)
+        # #nums[1] = 2, which is the dominant element.So, prefix_count and total_count changes as prefix_count = 1 , total_count = 2.
+        # Left subarray: [1, 2] → 2 appears once → 1 * 2 = 2, which is not greater than 2 (not dominant).
+        # Right subarray: [2, 2] → 2 appears twice → 2 * 2 = 4 > 2 (dominant).
+        # No valid split at i = 1
+#iteration 3 (i =2 , nums [i] = 2)
+        # #nums[1] = 2, which is the dominant element.So, prefix_count and total_count changes as prefix_count = 2 , total_count = 1.
+        # Left subarray: [1, 2, 2] → 2 appears twice → 2 * 2 = 4 > 3 (dominant).
+        # Right subarray: [2] → 2 appears once → 1 * 2 = 2 > 1 (dominant).
+        # Since 2 is dominant in both halves, we return i = 2.
 
 
 
